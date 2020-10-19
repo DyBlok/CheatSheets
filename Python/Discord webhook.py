a1b2c47bb12a934ec27f9ec48cf84c26c2482b6e
Dylan Blok <62259640+DyBlok@users.noreply.github.com>
2020-10-19 17:48:09 +0200

    Update Discord webhook.py

--- Python/Discord webhook.py
+++ Python/Discord webhook.py
@@ -14,7 +14,10 @@ def post_discord(Field2):
     embed.add_embed_field(name='Field 2', value=Field2)
 #Use inline false to get the field underneath the others
     embed.add_embed_field(name='Field 3', value='[Field 3 text](http://example.com)',inline=False)
+#The small picture
     embed.set_thumbnail(url='https://avatars3.githubusercontent.com/u/62259640?s=460&u=6124b4b9ed87018e1dfe84a2138393011154cd50&v=4')
+#The big picture
+    embed.set_image(url='https://avatars3.githubusercontent.com/u/62259640?s=460&u=6124b4b9ed87018e1dfe84a2138393011154cd50&v=4')
     embed.set_footer(text='My Footer', icon_url='https://avatars3.githubusercontent.com/u/62259640?s=460&u=6124b4b9ed87018e1dfe84a2138393011154cd50&v=4')
     embed.set_timestamp()
 #Adding embeds and executing the webhook
